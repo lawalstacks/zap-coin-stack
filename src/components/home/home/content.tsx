6f1f16ded23187fd1c052b835d0d0705af651784
QuadriBabs <babawalequadri2019@gmail.com>
2024-10-12 08:35:22 +0100

    update

--- src/components/home/home/content.tsx
+++ src/components/home/home/content.tsx
@@ -5,7 +5,7 @@ export const Content = () => {
       <div
         className="relative flex md:flex-col z-20">
           <div className="ml-10 px-5">
-          <img src={`/bg/content.png`} className="absolute  flex "/>
+            <div className="image-center"></div>
           </div>
     </div>
     <img src={`/animations/ZAP SITTING 2.gif`} className="ml-80 mt-20"/>
