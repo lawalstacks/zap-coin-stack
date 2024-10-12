b802996cfef1ba72f500bc18d93b0a755dac69a6
Kazeem Lawal <quazeemastic@gmail.com>
2024-10-12 22:45:49 +0100

    zapcoin updated

--- src/components/home/buy/index.tsx
+++ src/components/home/buy/index.tsx
@@ -2,7 +2,7 @@ import Form from '@/components/form'
 export const Buy = () => {
   return (
     <>
-      <div id="How To Buy" className="relative z-20 mt-[-10rem] max-sm:pb-[20rem] max-sm:pt-[25rem] sm:pb-[15rem] sm:pt-[20rem] overflow-hidden" style={{backgroundImage:"url(/bg/buy.jpg)"}}>
+      <div id="How To Buy" className="mt-[-10rem] max-sm:pb-[20rem] max-sm:pt-[25rem] sm:pb-[15rem] sm:pt-[20rem] overflow-hidden">
           
           <div className="proximanova relative scale-y-125 text-[white] px-[1rem] sm:px-[4rem] pb-[6rem] sm:pb-[6rem] pt-[3rem] sm:pt-[4rem] border-[0.7rem] border-black rounded-[4rem] w-[80vw] mx-auto lg:w-[60vw]" style={{backgroundImage:"linear-gradient(to bottom, #fadc96 , #d4faf8)"}}>
             <img src="/title/header4.png"  className="absolute z-[40] max-sm:w-[100%] max-sm:left-[0] max-md:w-[60%] max-md:left-[20%] md:w-[50%] md:left-[25%] top-[-2rem] 2xl:top-[-3rem]"></img>
--- src/components/home/home/content.tsx
+++ src/components/home/home/content.tsx
@@ -5,11 +5,14 @@ export const Content = () => {
       <div className="flex absolute right-60">
         <div className="absolue w-[500px]  h-[590px] left-0">
         <div className="absolute rounded-xl bg-[#F3C035]  inner-border-3 inner-border-[#EFB746] top-5 left-5"><img src={`/title/ZAPPY BRINGS.png`} /></div>
+          <div className="absolute rounded-xl bg-[]"></div>
         </div>
       </div>
-      <div className="zapChair mt-[400px]">
-    <img src={`/animations/ZAP.gif`} width={600} className="absolute bottom-40"/>
-  
+      <div className="">
+        <div className="">
+        <img src={`/other/23.png`} width={500} className=" mt-[400px]"/>
+        <img src={`/animations/ZAP.gif`} width={900}  className="absolute bottom-40"/>
+        </div>
       </div>
     </div>
     </>
